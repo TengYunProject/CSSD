06ce0e3a2c430c0c75827697cafcccb8e64b95ab
liushengning <517471114@qq.com>
2015-05-20 17:07:59 +0800

    用户信息维护

--- CSSD.Server.DataModel/ManagerBenchModel/UserGroupModel.cs
+++ CSSD.Server.DataModel/ManagerBenchModel/UserGroupModel.cs
@@ -1,6 +1,9 @@
 ﻿using CSSD.Server.BaseClass;
+using CSSD.Server.DataBase;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -76,7 +79,19 @@ namespace CSSD.Server.DataModel.ManagerBenchModel
 
         public bool DelayedInsert(out string errorString, string connectionString)
         {
-            throw new NotImplementedException();
+            errorString = string.Empty;
+            string sqlStr = "insert into UserGroup(" +
+                "UserGroupDesc,PermissionID,UserGroupName,CreateUserID,CreateDateTime,AccountCode,SpellCode" +
+            ")values(@UserGroupDesc,@PermissionID,@UserGroupName,@CreateUserID,@CreateDateTime,@AccountCode,@SpellCode);";
+            SqlParameter[] parameters = {   new SqlParameter("@UserGroupDesc", UserGroupDesc),
+											new SqlParameter("@PermissionID", PermissionID),
+                                            new SqlParameter("@UserGroupName", UserGroupName),
+											new SqlParameter("@CreateUserID", CreateUserID),
+                                            new SqlParameter("@CreateDateTime", CreateDateTime),
+											new SqlParameter("@AccountCode", AccountCode),
+                                            new SqlParameter("@SpellCode", SpellCode)};
+            int result = SqlDatabaseManager<SiteTypeModel>.ExecuteNonQuery(out errorString, connectionString, sqlStr, parameters);
+            return result <= 0 ? false : true;
         }
 
         public object DelayedSelectAll(out string errorString, string connectionString)
@@ -86,12 +101,29 @@ namespace CSSD.Server.DataModel.ManagerBenchModel
 
         public System.Data.DataTable DelayedSelectAllToTable(out string errorString, string connectionString)
         {
-            throw new NotImplementedException();
+            errorString = string.Empty;
+            string sqlStr = "select * from UserGroup";
+            DataTable dt = SqlDatabaseManager<DataTable>.FillToDataTable(out errorString, connectionString, sqlStr);
+            return dt;
         }
 
         public bool DelayedUpdate(out string errorString, string connectionString)
         {
-            throw new NotImplementedException();
+            errorString = string.Empty;
+            string sqlStr = "update UserGroup set" +
+                "UserGroupID=@UserGroupID,UserGroupDesc=@UserGroupDesc,PermissionID=@PermissionID,UserGroupName=@UserGroupName,"
+                + "CreateUserID=@CreateUserID,CreateDateTime=@CreateDateTime,AccountCode@AccountCode,SpellCode=@SpellCode "
+                + " where UserGroupID=@UserGroupID;";
+            SqlParameter[] parameters = {   new SqlParameter("@UserGroupID", UserGroupID),			
+                                            new SqlParameter("@UserGroupDesc", UserGroupDesc),
+											new SqlParameter("@PermissionID", PermissionID),
+                                            new SqlParameter("@UserGroupName", UserGroupName),
+											new SqlParameter("@CreateUserID", CreateUserID),
+                                            new SqlParameter("@CreateDateTime", CreateDateTime),
+											new SqlParameter("@AccountCode", AccountCode),
+                                            new SqlParameter("@SpellCode", SpellCode)};
+            int result = SqlDatabaseManager<SiteTypeModel>.ExecuteNonQuery(out errorString, connectionString, sqlStr, parameters);
+            return result <= 0 ? false : true;
         }
         #endregion
 
